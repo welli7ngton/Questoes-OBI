693db9bb3e7dd3f3f6628ec0412c57e7382b16ba
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-24 17:29:03 -0300

    adição da primeira resolução

---
+++ 2014/Fase 1/cartas.py
@@ -0,0 +1,24 @@
+cartas = input().split(" ")
+
+cartas = list(map(int, cartas))
+
+for a in range(0, len(cartas) - 1):
+    if cartas[a] < cartas[a + 1]:
+        r = True
+    else:
+        r = False
+        break
+
+if r == False:
+    for a in range(0, len(cartas) - 1):
+        if cartas[a + 1] < cartas[a]:
+            r = True
+        else:
+            r = False
+            print("N")  
+            break 
+    if r == True:
+        print("D")
+
+else:
+    print("C")    
