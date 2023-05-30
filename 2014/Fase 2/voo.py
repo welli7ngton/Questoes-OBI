4553e0145775958accc932232d96fc14bf6784ba
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-30 20:11:02 -0300

    adição de atividade

--- 2014/Fase 2/voo.py
+++ 2014/Fase 2/voo.py
@@ -14,30 +14,11 @@ A saída consiste de uma linha, informando a duração do voo em minutos e quant
 
 # Entrada
 # Pa    Cb                             Pb    Ca                diferença de fusos:
-# 10:00 22:00                          10:00 18:00                    02hrs       
-# 10:00 20:00 hora a 10hrs de voo      08:00 18:00 hora a      tempo de voo:
-# 12:00 22:00 hora b 10hrs de voo      10:00 20:00 hora b             10hrs
-
-# Saída
-# 600 2 | 10hrs 2hrs
-
-# Entrada
-# Pa    Cb                             Pb    Ca                diferença de fusos:
-# 17:00 13:00                          17:00 23:00                    05hrs
-# 17:00 18:00 hora a                   17:00 18:00 hora a      tempo de voo:
-# 12:00 13:00 hora b                   22:00 23:00 hora b             10hrs
-
-# Saída
-# 60 -5 | 1hrs 2hrs
-
-# Entrada
-# Pa    Cb                             Pb    Ca                diferença de fusos:
 # 18:00 12:00                          18:00 14:00                    11hrs
 # 18:00 01:00 hora a                   07:00 14:00 hora a      tempo de voo:
 # 05:00 12:00 hora b                   18:00 01:00 hora b             7hrs
 
-# Saída
-# 420 11 | 7hrs 11hrs
+    
 
 
 
@@ -60,62 +41,56 @@ horarios.append(int(c_A[:2]))
 xa = horarios[0] - horarios[1]
 xb = horarios[2] - horarios[3]
 
-delta = 0
-if xa < 0 and xb < 0:
+if horarios[0] > horarios[1] and horarios[2] > horarios[3]:
+    delta = 0
 
     if xa > xb:
-
-        while True:
-            if xa != xb:
-                xa -= 1
-                xb +=1 
-                delta += 1
-            else:
-                delta *= -1
-                break
-    else:
-
-        while True:
-            if xa != xb:
-                xb -= 1
-                xa +=1 
-                delta += 1
-            else:   
-                break
-    chegada_a = horarios[1] + (delta * -1)
-    print((chegada_a - horarios[0])* 60, delta)
-else:
-    if xa > xb:
-
-        while True:
-            if xa != xb:
-                xa -= 1
-                xb +=1 
-                delta += 1
-            else:
-                delta = xa
-                break
+        while xa != xb:
+            xa -= 1
+            xb += 1
+            delta += 1
+        delta = delta * -1 if xb < 0 else delta
         
     else:
+        while xa != xb:
+            xa += 1
+            xb -= 1
+            delta += 1
+        delta = delta * -1 if xb > 0 else delta
+    
+    delta = xa
+    
 
-        while True:
-            if xa != xb:
-                xb -= 1
-                xa +=1 
-                delta += 1
-            else:
-                delta = xa  
-                break
-    
-    # definindo a hora de chegada de b no fuso horario de b
-    tempo_voo = horarios[3] - delta
-    fuso = tempo_voo - horarios[2]
+    pbha = (horarios[3] + delta) - 12
+    fuso = horarios[2] - pbha
+    print(fuso)
 
-    print(tempo_voo * 60, fuso)
-    
-    
-    
-    
+
+
+
+else:
+
+    delta = 0
+
+    if xa > xb:
+        while xa != xb:
+            xa -= 1
+            xb += 1
+            delta += 1
+        delta = delta * -1 if xb > 0 else delta
+        
+    else:
+        while xa != xb:
+            xa += 1
+            xb -= 1
+            delta += 1
+        delta = delta * -1 if xb < 0 else delta
+
+
+    cbha = horarios[1] + delta
+        
+    t_voo = (cbha - horarios[0]) * 60
+    print(t_voo, delta * -1)  
 
 
   
