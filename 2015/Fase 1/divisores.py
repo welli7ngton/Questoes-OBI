33eb3d7e1d5d91c4114a59045c2d523594a302a8
Wellington Silva <wellingtonasilva45@gmail.com>
2023-09-21 23:07:36 -0300

    corrigindo código

--- 2015/Fase 1/divisores.py
+++ 2015/Fase 1/divisores.py
@@ -1,45 +1,47 @@
 # https://olimpiada.ic.unicamp.br/pratique/ps/2015/f1/divisores/
 
 
-def ocorrencia(l=[],exp=[],c=0, p=2,contador=0):
+def ocorrencia(_l=[], exp=[], c=0, p=2, contador=0):
     while True:
-        if p not in l:
+        if p not in _l:
             p += 1
         break
-        
-    if c == len(l) + 1:
+
+    if c == len(_l) + 1:
         return exp
-    
-    for numero in l:
+
+    for numero in _l:
         if numero == p:
-            contador +=1
+            contador += 1
     if contador > 0:
         exp.append(contador)
-        
-    c += 1
-    p += 1    
-    return ocorrencia(l,exp,c,p)
 
-def prim(n,l=[],div=2):
+    c += 1
+    p += 1
+    return ocorrencia(_l, exp, c, p)
+
+
+def prim(n, _l=[], div=2):
     if n == 1:
-        return l
-    
-    if n %div == 0:
+        return _l
+
+    if n % div == 0:
         n = n/div
-        l.append(div)
-        
+        _l.append(div)
+
     else:
         div += 1
 
-    return prim(n,l,div)
+    return prim(n, _l, div)
+
 
 n = int(input())
 
 exp = ocorrencia(prim(n))
 
 r = 1
-for a in range(0,len(exp)):
-    
+for a in range(0, len(exp)):
+
     r *= exp[a] + 1
 
 print(r)
